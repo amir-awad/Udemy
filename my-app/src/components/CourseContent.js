340b74408a8f7a21afe75694f08df703dc012e45
amir-awad <amirtarek04@gmail.com>
2022-09-14 05:42:22 +0200

    add content "more sections" button

--- my-app/src/components/CourseContent.js
+++ my-app/src/components/CourseContent.js
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentItem from "./ContentItem";
 
 function CourseContent({ content, sectionsCount, lecturesCount, totalLength }) {
-  const getContent = () => {
-    return content.map((item) => {
-      return <ContentItem item={item} key={item}/>;
+  const [moreContent, setMoreContent] = useState(true);
+
+  const getContent = (shownContent) => {
+    return shownContent.map((item) => {
+      return <ContentItem item={item} key={item} />;
     });
   };
 
-  return (
+  const clickHandle = () => {
+    setMoreContent(false);
+  };
+
+  const displayContentButton = () => {
+    return moreContent ? (
+      <button className="moreContentButton" onClick={clickHandle}>
+        {content.length - 5} more sections
+      </button>
+    ) : (
+      <></>
+    );
+  };
+
+  const displayContent = () => {
+    return (
+      <>
+        <h6>Course Content</h6>
+        <div className="content">
+          <span className="contentCount">
+            {sectionsCount} sections - {lecturesCount} lectures - {totalLength}{" "}
+            total length
+          </span>
+        </div>
+        <div className="courseContent">
+          {!moreContent ? getContent(content) : getContent(content.slice(0, 5))}
+        </div>
+        {displayContentButton()}
+      </>
+    );
+  };
+
+  return content.length <= 5 ? (
     <>
       <h6>Course Content</h6>
       <div className="content">
@@ -17,8 +51,10 @@ function CourseContent({ content, sectionsCount, lecturesCount, totalLength }) {
           total length
         </span>
       </div>
-      <div className="courseContent">{getContent()}</div>
+      <div className="courseContent">{getContent(content)}</div>
     </>
+  ) : (
+    <>{displayContent()}</>
   );
 }
 
